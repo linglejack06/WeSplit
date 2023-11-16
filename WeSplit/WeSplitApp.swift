a119a15c62171b48596575a3d88ed65cc1b4d3a5
Jack Lingle <captainjack0818@gmail.com>
2023-11-16 17:59:35 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jack Lingle on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
